5a7c791b32436b22f716bac8f5aaaa8762a8988b
Houra Batool <hourabatool@gmail.com>
2023-11-20 21:38:53 +0500

    - list displayed model data

--- project_app/admin.py
+++ project_app/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from project_app.models import Project, Task, Category
 
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'due_date', 'project', 'assigned_to_list')
+
+    def assigned_to_list(self, obj):
+        return ", ".join([user.username for user in obj.assigned_to.all()])
+
+    assigned_to_list.short_description = 'Assigned To'
+
+
+
 admin.site.register(Project)
 admin.site.register(Category)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
