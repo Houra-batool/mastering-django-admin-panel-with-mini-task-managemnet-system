8ab6ef80b9d04a8f7db6ddb1fa5c398d02758999
Houra Batool <hourabatool@gmail.com>
2023-11-21 07:25:49 +0500

    - created django action

--- project_app/admin.py
+++ project_app/admin.py
@@ -8,12 +8,17 @@ class TaskAdmin(admin.ModelAdmin):
     list_filter = ('status', 'project__name', 'due_date')
     search_fields = ('title', 'description', 'assigned_to__username')
     date_hierarchy = 'due_date'
+    actions = ['mark_as_pending']
 
     def assigned_to_list(self, obj):
         return ", ".join([user.username for user in obj.assigned_to.all()])
 
     assigned_to_list.short_description = 'Assigned To'
 
+    def mark_as_pending(self, request, queryset):
+        queryset.update(status='PENDING')
+
+    mark_as_pending.short_description = "Mark selected tasks as pending"
 
 
 admin.site.register(Project)
