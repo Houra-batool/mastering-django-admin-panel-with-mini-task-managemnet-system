a82b60c4888952ef28163432db67e246471157eb
Houra Batool <hourabatool@gmail.com>
2023-11-21 08:05:26 +0500

    - prepopulated fields

--- project_app/admin.py
+++ project_app/admin.py
@@ -15,6 +15,7 @@ class TaskAdmin(admin.ModelAdmin):
     save_as = True
     save_on_top = True
     raw_id_fields = ("project",)
+    prepopulated_fields = {"description": ("title",)}
 
     def assigned_to_list(self, obj):
         return ", ".join([user.username for user in obj.assigned_to.all()])
